ed29ce0fc1718ab1823bd1ae631bc890b02f03e8
刘荣江 <774726704@qq.com>
2017-10-24 12:41:35 +0800

    add

---
+++ ic_crawler/broken_promises.py
@@ -0,0 +1,30 @@
+#coding=utf-8
+
+sxgg = {
+    "rowkey":           "md5",
+    "rowkeySource":     "",
+
+    "pname":            "被执行人姓名/名称",          #iname
+    "idcardNo":         "身份证号码/组织机构代码",     #cardNum
+    "sex":              "被执行人性别",              #sexy
+    "age":              "年龄",                     #age
+    "caseCode":         "案件号",                   #caseCode
+    "court":            "执行法院",                  #courtName
+    "province":         "省份",                     #areaName
+    "yjCode":           "执行依据文号",              #gistId
+    "yjdw":             "做出执行依据单位",           #courtName
+    "yiwu":             "生效法律文书确定的义务",      #duty
+    "lxqk":             "被执行人的履行情况",          #performance
+    "jtqx":             "失信被执行人行为具体情形",     #disruptTypeName
+    "sortTimeString":   "发布日期",                  #publishDate
+    "sortTime":         "发布时间",                  #publishDateStamp
+
+    "dataType":         "",                         #分类 sxgg
+    "sxggId":           "",                         #关联id
+    "postTime":         "",                         #法海特有
+    "body": ""          "",                         #内容，法海特有、重复信息
+    "lerepName":        "法定代表人或者负责人姓名",     #暂无、公司才有
+}
+
+
+
